851206e371109241f5dbc753781617e607019c21
Grinch <jcarew@vols.utk.edu>
2019-12-09 18:03:32 -0500

    Added file for main project file.

---
+++ nt_main.py
@@ -0,0 +1,7 @@
+# nt_main.py
+
+# This file is the "engine" of the project.  It will interact with the user
+# through the terminal to allow tracking of nutrition.  It will also allow
+# changes to stored data of foods.
+
+import nt_definition.py # contains 'Food' class as well as functions to manipulate them
