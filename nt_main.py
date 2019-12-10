02997271bb187f9aaaa83cb1481333d94aedbee4
Grinch <jcarew@vols.utk.edu>
2019-12-10 15:21:48 -0500

    Updated 'nt_main.py' to have example screen.  Added '__init__.py' to allow import of custom modules.

--- nt_main.py
+++ nt_main.py
@@ -4,4 +4,19 @@
 # through the terminal to allow tracking of nutrition.  It will also allow
 # changes to stored data of foods.
 
-import nt_definition.py # contains 'Food' class as well as functions to manipulate them
+from nt_definitions import * # contains 'Food' class as well as functions to manipulate them
+
+print("--Welcome to Nutrition Tracker!--")
+print("What would you like to do?")
+print("[1] Check current nutrition counts\n"
+      "[2] Update today's counts\n"
+      "[3] Add info for a food\n"
+      "[4] Edit info for a food\n"
+      "[5] Exit Program")
+
+choice = input("Enter number of choice: ")
+
+if choice == '1':
+    print("good")
+else:
+    print("bad")
