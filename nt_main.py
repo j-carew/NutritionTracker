c64f9495990936ebcc92025f1259495c82730299
Grinch <jcarew@vols.utk.edu>
2019-12-15 19:02:49 -0500

    Implemented 'curses' module

--- nt_main.py
+++ nt_main.py
@@ -1,22 +1,51 @@
-# nt_main.py
+#nt_main.py
 
 # This file is the "engine" of the project.  It will interact with the user
 # through the terminal to allow tracking of nutrition.  It will also allow
 # changes to stored data of foods.
 
 from nt_definitions import * # contains 'Food' class as well as functions to manipulate them
+import curses # For altering terminal screens
 
-print("--Welcome to Nutrition Tracker!--")
-print("What would you like to do?")
-print("[1] Check current nutrition counts\n"
-      "[2] Update today's counts\n"
-      "[3] Add info for a food\n"
-      "[4] Edit info for a food\n"
-      "[5] Exit Program")
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr("--Welcome to Nutrition Tracker!--\n\n")
 
-choice = input("Enter number of choice: ")
+    stdscr.addstr("What would you like to do?\n\n")
 
-if choice == '1':
-    print("good")
-else:
-    print("bad")
+    # The following string will be printed to the screen and ask the user to input
+    # a number to perform a corresponding action.
+
+    stdscr.addstr("[1] Check current nutrition counts\n"
+          "[2] Update today's counts\n"
+          "[3] Add info for a food\n"
+          "[4] Edit info for a food\n"
+          "[5] Exit Program\n")
+
+
+    # The following section currently needs work
+    choice = ''
+    while choice != '5':
+
+        choice = stdscr.getkey()
+
+        if choice == '1':
+            stdscr.clear()
+            stdscr.addstr("Option 1")
+        elif choice == '2':
+            stdscr.clear()
+            stdscr.addstr("Option 2")
+        elif choice == '3':
+            stdscr.clear()
+            stdscr.addstr("Option 3")
+        elif choice == '4':
+            stdscr.clear()
+            stdscr.addstr("Option 4")
+        elif choice == '5':
+            pass
+        else:
+            stdscr.addstr("Invalid option.  Please try again.\n")
+
+    stdscr.refresh()
+
+curses.wrapper(main)
